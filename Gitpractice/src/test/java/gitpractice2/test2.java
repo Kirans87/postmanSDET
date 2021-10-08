7befd56d93727982c6b87eb65207726e873e05f5
Kirans87 <kiranstark07@gmail.com>
2021-10-08 15:41:15 +0530

    commit

--- Gitpractice/src/test/java/gitpractice2/test2.java
+++ Gitpractice/src/test/java/gitpractice2/test2.java
@@ -9,12 +9,8 @@ public class test2 {
 		System.out.println("Anup 1");
 
 		System.out.println("new push3");
-<<<<<<< HEAD
-		System.out.println("new push4");
-=======
-		
 
->>>>>>> 451b41044a72a9e5e51b5d4d12c698228878f045
+		System.out.println("new push4");
 	}
 
 }
